d45d2d8df0f0b731bac34e70fcbfcdee2e77d839
thejainlabs <thejainlabs@gmail.com>
2020-03-07 22:39:48 +0530

    Initial Commit

---
+++ Simple Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Neelesh Jain on 07/03/20.
+//  Copyright © 2020 Neelesh Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
